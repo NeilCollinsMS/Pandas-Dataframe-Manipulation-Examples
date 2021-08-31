b5be884f1e820eeb92d70aebf53a3bebeb36da60
Neil Collins <68830945+NeilCollinsMS@users.noreply.github.com>
2021-08-31 16:48:46 -0700

    Update samples.py

--- samples.py
+++ samples.py
@@ -1,2 +1,5 @@
 # Code to strip HTML formatting from a dataframe column
 df['column'] = df['column'].apply(lambda x: re.sub('<[^<]+?>', '', x))
+
+# Collapse columns to fill null values
+df.ffill(1).iloc[:,-1]
