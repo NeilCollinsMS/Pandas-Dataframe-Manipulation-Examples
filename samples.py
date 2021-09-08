4efac53c1dc19c0919a2a39bfc93eb7b181c3329
Neil Collins <68830945+NeilCollinsMS@users.noreply.github.com>
2021-09-08 13:49:22 -0700

    Create target and drop excess

--- samples.py
+++ samples.py
@@ -3,3 +3,10 @@ df['column'] = df['column'].apply(lambda x: re.sub('<[^<]+?>', '', x))
 
 # Collapse columns to fill null values
 df.ffill(1).iloc[:,-1]
+
+# Create a target feature column
+df.loc[df['Status'] == 'Yes', 'Target'] = 1
+df.loc[df['Status'] == 'No', 'Target'] = 0
+
+# Drop excess columns
+df.drop(columns = ['Column A', 'Column W'])
